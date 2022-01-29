0ebf9e6236d28abf49d2b537bc225c6d9ac9373c
MonadWizard <monad.wizard.r@gmail.com>
2022-01-29 21:57:45 +0600

    simple_registration_ORM

---
+++ demo_mail_registration_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DemoMailRegistrationAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'demo_mail_registration_app'
