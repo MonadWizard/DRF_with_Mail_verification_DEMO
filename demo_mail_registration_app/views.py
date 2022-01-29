4cc8adeea0e0f992433e7d132c08c9c6bed6c4ef
MonadWizard <monad.wizard.r@gmail.com>
2022-01-30 00:33:32 +0600

    simple_registrationORM_mail_sended

--- demo_mail_registration_app/views.py
+++ demo_mail_registration_app/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, views
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
 
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 
 
 # Create your views here.
@@ -17,6 +22,47 @@ class RegisterView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         user_data = serializer.data
+
+# Token passing
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
         
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        
+        email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
+        
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
+
         return Response(user_data, status=status.HTTP_201_CREATED)
 
+
+
+class VerifyEmail(views.APIView):
+    # serializer_class = EmailVerificationSerializer
+
+    # token_param_config = openapi.Parameter(
+    #     'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    # @swagger_auto_schema(manual_parameters=[token_param_config])
+    # def get(self, request):
+    #     token = request.GET.get('token')
+    #     try:
+    #         payload = jwt.decode(token, settings.SECRET_KEY)
+    #         user = User.objects.get(id=payload['user_id'])
+    #         if not user.is_verified:
+    #             user.is_verified = True
+    #             user.save()
+    #         return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+    #     except jwt.ExpiredSignatureError as identifier:
+    #         return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+    #     except jwt.exceptions.DecodeError as identifier:
+    #         return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self):
+        pass
